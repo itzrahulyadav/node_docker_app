72f664b61a1e859ce9c9d2cf4e7f6edc6000cd91
Rahul Kumar Yadav <65400893+itzrahulyadav@users.noreply.github.com>
2023-02-19 01:22:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const homeroutes = require("./routes/home")
 app.set('view engine', 'ejs')
 app.use(express.json())
 
-app.use("/",homeroutes)
-
-app.use("/jokes",getDarkJokes)
+app.use("/",getDarkJokes)
 
 app.listen(3000,()=>{
     console.log("server is running,catch it if you can!!!")
